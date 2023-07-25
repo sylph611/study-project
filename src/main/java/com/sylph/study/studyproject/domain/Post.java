1fa08e58fd0a76f7b1ff0741b58f31bb98b4020d
sylph611 <ccg0611a@gmail.com>
2023-07-25 23:32:36 +0900

    Post Entity 생성 / application.yaml 수정.

--- src/main/java/com/sylph/study/studyproject/domain/Post.java
+++ src/main/java/com/sylph/study/studyproject/domain/Post.java
@@ -1,13 +1,23 @@
 package com.sylph.study.studyproject.domain;
 
 import com.sylph.study.studyproject.base.BaseEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
 import org.hibernate.annotations.Comment;
 
+@Entity
+@Getter
+@Table(name = "post")
 public class Post extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Comment("PK")
     private long id;
+
     @Comment("게시글 제목")
     private String title;
+
     @Comment("게시글 내용")
     private String content;
 }
