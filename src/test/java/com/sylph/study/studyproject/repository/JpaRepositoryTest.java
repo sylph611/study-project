8963f37cdbf63cc64b72eeed65013185c639092f
sylph611 <ccg0611a@gmail.com>
2023-07-27 00:37:01 +0900

    --

--- src/test/java/com/sylph/study/studyproject/repository/JpaRepositoryTest.java
+++ src/test/java/com/sylph/study/studyproject/repository/JpaRepositoryTest.java
@@ -5,13 +5,17 @@ import com.sylph.study.studyproject.domain.Post;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+//@ActiveProfiles
+//@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @DisplayName("JPA 연결 테스트")
 @Import(JpaConfig.class)
 @DataJpaTest
